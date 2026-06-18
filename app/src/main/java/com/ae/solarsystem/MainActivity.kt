5099d087599241d61d8346a9ca0d6e4b126447f2
abdo-essam <abdo.e.mosa@gmail.com>
2026-06-18 22:41:38 +0300

    Clean up unused imports in `MainActivity.kt`.
    - Remove unused `clip` and `graphicsLayer` imports from the Compose UI library.
    - Remove the unused `AsyncImage` import from the Coil library.

--- app/src/main/java/com/ae/solarsystem/MainActivity.kt
+++ app/src/main/java/com/ae/solarsystem/MainActivity.kt
@@ -14,10 +14,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -27,7 +25,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
